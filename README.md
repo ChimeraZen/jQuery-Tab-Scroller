04ed42058c94cca6aa4c0da2577372789b778240
Chimera Zen <chimera.zen@gmail.com>
2017-12-13 15:26:53 -0500

    Changes suggested by community
    Version: 1.1.0
    
    -  leftscrolled changed to left_scrolled
    -  changed tab_switch() to toggle_class()
      -  switched from .css() to .toggleClass()
    -  removed CSS styling for .prev
      -  added styling for .visible and .hidden

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # jQuery-Tab-Scroller
 
-Version: 1.0.4<br>
+Version: 1.1.0<br>
 Author: Chimera.Zen <a href="mailto:chimera.zen@gmail.com"><chimera.zen@gmail.com></a><br/>
 Description: jQuery Tab Scroller is an app that uses clases and onclick functions to scroll a defined distance<br>
 Example: <a href="https://jsfiddle.net/ChimeraZen/g8hw0r8u/">jsfiddle.net</a>
--- tab-scroller.js
+++ tab-scroller.js
@@ -2,19 +2,19 @@
  * Name:        jQuery Tab Scroller
  * Description: A tab scroller using jQuery
  * @package     Chimera Apps
- * @version     1.0.4
+ * @version     1.1.0
  * @author      Chimera.Zen
  * @copyright   Copyright (c) 2017, Chimera.Zen
- * @link        https://github.com/ChimeraZen/Chimera-Core-JS
+ * @link        https://github.com/ChimeraZen
  * @license     http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
+'use strict';
 var scroll_distance = 275,    // Distance .tab-container should scroll when <i> is clicked
     animate_speed   = 400;    // Speed at which .tab-container should animate the scroll
 
 /** Load the scroller details **/
 function get_scroll_details(scroller) {
-  "use strict";
   var tab_true_width  = Math.round(scroller.siblings('.tab-container').children('ul').width()),
       container_width = Math.round(scroller.siblings('.tab-container').width()),
       left_scrolled   = Math.round(scroller.siblings('.tab-container').scrollLeft()),
@@ -29,32 +29,23 @@ function get_scroll_details(scroller) {
 }
 
 /** Tab Scroller **/
-function tab_switch(scroller) {
-  "use strict";
-  var scrolls = get_scroll_details(scroller);
-  if (scrolls.leftscrolled === 0) {
-    scrolls.scroll_next.css("visibility", "visible");
-    scrolls.scroll_prev.css("visibility", "hidden");
-  } else if (scrolls.remaining === 0) {
-    scrolls.scroll_next.css("visibility", "hidden");
-    scrolls.scroll_prev.css("visibility", "visible");
-  } else {
-    scrolls.scroll_next.css("visibility", "visible");
-    scrolls.scroll_prev.css("visibility", "visible");
-  }
+function toggle_class(scroller) {
+  var scrolls     = get_scroll_details(scroller),
+      next_visible = scrolls.leftscrolled === 0 || scrolls.remaining !== 0,
+      prev_visible = scrolls.remaining === 0 || scrolls.left_scrolled !== 0;
+  scrolls.scroll_next.toggleClass('visible', next_visible).toggleClass('hidden', !next_visible);
+  scrolls.scroll_prev.toggleClass('visible', prev_visible).toggleClass('hidden', !prev_visible);
 }
 
 /** Animate and check if <i> visibility needs to switch **/
 function scroll_it(scroller, scroll) {
-  "use strict";
   scroller.siblings('.tab-container').animate({scrollLeft: scroll}, animate_speed, function () {
-    tab_switch(scroller);
+    toggle_class(scroller);
   });
 }
 
 /** Animate & Scroll on Click **/
 $('.scroller.next').click(function () {
-  "use strict";
   var scroller  = $(this),
       scrolls   = get_scroll_details(scroller);
   if (scrolls.remaining >= scroll_distance) {
@@ -65,7 +56,6 @@ $('.scroller.next').click(function () {
 });
 
 $('.scroller.prev').click(function () {
-  "use strict";
   var scroller  = $(this),
       scrolls   = get_scroll_details(scroller);
   if (scrolls.leftscrolled !== 0) {
